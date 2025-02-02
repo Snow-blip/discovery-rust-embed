193f602ad766a20c702185dfdb41a6bbb7dc4fcd
Snow <48936193+Snow-blip@users.noreply.github.com>
2025-02-02 11:22:49 +0100

    5.6 The challenge

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,28 +3,50 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use rtt_target::{rtt_init_print, rprintln};
+use rtt_target::rtt_init_print;
 use panic_rtt_target as _;
-use microbit::board::Board;
-use microbit::hal::timer::Timer;
-use microbit::hal::prelude::*;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, Timer},
+};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    let mut board = Board::take().unwrap();
 
+    let board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
+    let show_time = 100_u32;
+    let dark_time = 100_u32;
+    let mut display = Display::new(board.display_pins);
+    let light_it_all = [
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+    ];
+    // Show light_it_all for 1000ms just because
+    display.show(&mut timer, light_it_all, 1000_u32);
+    // clear the display
+    display.clear();
+    timer.delay_ms(1000_u32);
 
-    board.display_pins.col1.set_low().unwrap();
-    let mut row1 = board.display_pins.row1;
+    const ROW_COUNT:usize = 5;
+    const COL_COUNT:usize = 5;
+    let mut image_matrix = [[0;COL_COUNT];ROW_COUNT];
+    let light_sequence = [[0,0],[0,1],[0,2],[0,3],[0,4],[1,4],[2,4],[3,4],[4,4],[4,3],[4,2],[4,1],[4,0],[3,0],[2,0],[1,0]];
 
     loop {
-        row1.set_low().unwrap();
-        rprintln!("Dark!");
-        timer.delay_ms(1_000_u16);
-        row1.set_high().unwrap();
-        rprintln!("Light!");
-        timer.delay_ms(1_000_u16);
+        for [row,col] in light_sequence {
+            // Show the current matrix
+            image_matrix[row][col] = 1;
+            display.show(&mut timer, image_matrix, show_time);
+            // clear the matrix and display again
+            image_matrix[row][col] = 0;
+            display.clear();
+            timer.delay_ms(dark_time);
+        }  
     }
 }
