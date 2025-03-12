5619dbb325a032a41ee35c64119c8ed8cb2bbcd8
Snow <48936193+Snow-blip@users.noreply.github.com>
2025-03-12 17:14:24 +0100

    WIP: 8.5. The challenge

--- microbit/src/08-i2c/src/main.rs
+++ microbit/src/08-i2c/src/main.rs
@@ -1,4 +1,3 @@
-#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
@@ -6,47 +5,113 @@ use cortex_m_rt::entry;
 use rtt_target::{rtt_init_print, rprintln};
 use panic_rtt_target as _;
 
-use microbit::hal::prelude::*;
+use cortex_m::register::basepri::write;
+use embedded_hal_nb::serial;
+use core::fmt::Write;
+use heapless::Vec;
+use core::str::from_utf8;
 
 #[cfg(feature = "v1")]
 use microbit::{
     hal::twi,
     pac::twi0::frequency::FREQUENCY_A,
+    hal::prelude::*,
+    hal::uart,
+    hal::uart::{Baudrate, Parity},
 };
 
 #[cfg(feature = "v2")]
 use microbit::{
     hal::twim,
     pac::twim0::frequency::FREQUENCY_A,
+    hal::prelude::*,
+    hal::uarte,
+    hal::uarte::{Baudrate, Parity},
 };
 
-const ACCELEROMETER_ADDR: u8 = 0b0011001;
-const MAGNETOMETER_ADDR: u8 = 0b0011110;
+#[cfg(feature = "v1")]
+use embedded_io::Read;
+#[cfg(feature = "v2")]
+use embedded_hal_nb::serial::Read;
 
-const ACCELEROMETER_ID_REG: u8 = 0x0f;
-const MAGNETOMETER_ID_REG: u8 = 0x4f;
+#[cfg(feature = "v2")]
+mod serial_setup;
+#[cfg(feature = "v2")]
+use serial_setup::UartePort;
+
+use lsm303agr::{
+    AccelOutputDataRate, Lsm303agr,
+};
 
 #[entry]
 fn main() -> ! {
+    // This is necessary
     rtt_init_print!();
+
+    // take ownership of the board
     let board = microbit::Board::take().unwrap();
 
-
+    // create i2c bus?
     #[cfg(feature = "v1")]
-    let mut i2c = { twi::Twi::new(board.TWI0, board.i2c.into(), FREQUENCY_A::K100) };
+    let i2c = { twi::Twi::new(board.TWI0, board.i2c.into(), FREQUENCY_A::K100) };
 
     #[cfg(feature = "v2")]
-    let mut i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+    let i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+    rprintln!("i2c bus created");
 
-    let mut acc = [0];
-    let mut mag = [0];
+    // setup serial communication through uart/uarte
+    #[cfg(feature = "v1")]
+    let mut serial = {
+        uart::Uart::new(
+            board.UART0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        )
+    };
 
-    // First write the address + register onto the bus, then read the chip's responses
-    i2c.write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc).unwrap();
-    i2c.write_read(MAGNETOMETER_ADDR, &[MAGNETOMETER_ID_REG], &mut mag).unwrap();
+    #[cfg(feature = "v2")]
+    let mut serial = {
+        let serial = uarte::Uarte::new(
+            board.UARTE0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        );
+        UartePort::new(serial)
+    };
+    rprintln!("Serial communication created");
 
-    rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
-    rprintln!("The magnetometer chip's id is: {:#b}", mag[0]);
+    // setup sensor connection
+    // requires resetting the microbit
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+    rprintln!("Sensor connected");
 
-    loop {}
+    // A buffer with 32 bytes of capacity
+    let mut buffer: Vec<u8, 32> = Vec::new();
+
+    loop {
+        buffer.clear();
+        let mut eol= false;
+        while !eol {
+            let read_byte=nb::block!(serial.read()).unwrap();
+            if read_byte == 13 {
+                eol=true;
+            } else {
+                match buffer.push(read_byte) {
+                    Ok(_) => (),
+                    Err(failed_byte) => {
+                        write!(serial,"Buffer full, could not write {} to internal buffer. Clearing buffer.\n\r",failed_byte).unwrap();
+                        buffer.clear();
+                    },
+                }
+            }
+        }
+        let data = sensor.accel_data().unwrap();
+        // serial print
+        write!(serial,"Acceleration: x {} y {} z {}", data.x, data.y, data.z).unwrap();
+        nb::block!(serial.flush()).unwrap()
+    }
 }
