c44c767df710224a57669f9e6fd7bb48db81a2a5
Snow <48936193+Snow-blip@users.noreply.github.com>
2025-04-02 11:39:01 +0200

    WIP: 10. Determined x direction: along the longer edge of microbit

--- microbit/src/10-punch-o-meter/src/main.rs
+++ microbit/src/10-punch-o-meter/src/main.rs
@@ -3,13 +3,50 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use rtt_target::rtt_init_print;
+use rtt_target::{rtt_init_print, rprintln};
 use panic_rtt_target as _;
+
+#[cfg(feature = "v1")]
+use microbit::{
+    hal::twi,
+    pac::twi0::frequency::FREQUENCY_A,
+    hal::prelude::*,
+};
+
+#[cfg(feature = "v2")]
+use microbit::{
+    hal::twim,
+    pac::twim0::frequency::FREQUENCY_A,
+    hal::prelude::*,
+};
+
+use lsm303agr::{
+    AccelOutputDataRate, Lsm303agr,
+};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
 
-    loop {}
+    // create i2c bus?
+    #[cfg(feature = "v1")]
+    let i2c = { twi::Twi::new(board.TWI0, board.i2c.into(), FREQUENCY_A::K100) };
+
+    #[cfg(feature = "v2")]
+    let i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+    rprintln!("i2c bus created");
+
+    // setup sensor connection
+    // requires resetting the microbit
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+    rprintln!("Sensor connected");
+
+    loop {
+        let data = sensor.accel_data().unwrap();
+        rprintln!("Acceleration: x {} y {} z {}\n\r", 
+            data.x, data.y, data.z);
+    }
 }
